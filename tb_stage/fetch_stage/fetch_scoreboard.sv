c7aa95b7d4b87b1d99f8c8081227c33ab83ee56b
Adrian's Linux server <patrick.pineda@torontomu.ca>
2026-03-10 18:03:37 -0400

    Add expected_pc

--- tb_stage/fetch_stage/fetch_scoreboard.sv
+++ tb_stage/fetch_stage/fetch_scoreboard.sv
@@ -3,13 +3,14 @@ class fetch_scoreboard;
     mailbox #(fetch_txn) drv_mbx;
     mailbox #(logic [31:0]) mon_mbx;
 
-    logic [31:0] model_pc;
+    logic [31:0] expected_pc;
+    logic [31:0] next_expected_pc;
 
     function new(mailbox #(fetch_txn) drv_mbx,
                  mailbox #(logic [31:0]) mon_mbx);
         this.drv_mbx = drv_mbx;
         this.mon_mbx = mon_mbx;
-        model_pc = 0;
+        expected_pc = 0;
     endfunction
 
     task run();
@@ -20,23 +21,25 @@ class fetch_scoreboard;
             drv_mbx.get(txn);
             mon_mbx.get(dut_pc);
 
-            // Model behavior
+            // Compute next PC
+            next_expected_pc = expected_pc;
+
             if (!txn.stall) begin
                 case(txn.pc_src)
-                    2'd0: model_pc = model_pc + 4;
-                    2'd1: model_pc = txn.pred_pc_target;
-                    2'd2: model_pc = txn.pc_plus4_ex;
-                    2'd3: model_pc = txn.pc_target_ex;
+                    2'd0: next_expected_pc = expected_pc + 4;
+                    2'd1: next_expected_pc = txn.pred_pc_target;
+                    2'd2: next_expected_pc = txn.pc_plus4_ex;
+                    2'd3: next_expected_pc = txn.pc_target_ex;
                 endcase
             end
 
-            if (model_pc !== dut_pc) begin
-                $error("Mismatch! Model=%h DUT=%h",
-                        model_pc, dut_pc);
-            end
-            else begin
-                $display("PASS: PC=%h", dut_pc);
-            end
+            // Compare current DUT output with previous expected
+            if (expected_pc !== dut_pc)
+                $error("Mismatch! Model=%h DUT=%h \n", expected_pc, dut_pc);
+            else
+                $display("PASS: PC=%h \n", dut_pc);
+
+            expected_pc = next_expected_pc;
         end
     endtask
 
