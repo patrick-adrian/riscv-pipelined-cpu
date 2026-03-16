432d12fa5f8bf7694a99441591138d32c3e4b6e2
patrick-adrian <patrick.pineda@torontomu.ca>
2026-03-16 14:03:03 -0400

    Constraint random PCs for word-alignment

--- tb/env/fetch/fetch_txn.sv
+++ tb/env/fetch/fetch_txn.sv
@@ -8,6 +8,12 @@ class fetch_txn;
     rand logic [31:0] pc_plus4_ex;
     rand logic [31:0] pred_pc_target;
 
+    constraint pc_align {
+        pc_target_ex[1:0] == 2'b00;
+        pc_plus4_ex[1:0] == 2'b00;
+        pred_pc_target[1:0] == 2'b00;
+    }
+
     function void display(int cycle);
         $display("[CYCLE %0d] DRV TXN[%0d]: src=%0d stall=%0b ex_t=%h ex_p4=%h pred=%h",
                  cycle, id, pc_src, stall, pc_target_ex,
